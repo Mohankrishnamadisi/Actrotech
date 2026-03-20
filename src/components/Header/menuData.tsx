d73fa742d2abe60ccdfe570b8960548072368a25
Mohankrishnamadisi <mohankrishnamadisi@gmail.com>
2026-03-20 23:56:55 +0530

    menu bar changed

--- src/components/Header/menuData.tsx
+++ src/components/Header/menuData.tsx
@@ -13,62 +13,50 @@ const menuData: Menu[] = [
     path: "/about",
     newTab: false,
   },
-  {
-    id: 33,
-    title: "Blog",
-    path: "/blog",
-    newTab: false,
-  },
-  {
-    id: 3,
-    title: "Support",
-    path: "/contact",
-    newTab: false,
-  },
-  {
+   {
     id: 4,
-    title: "Pages",
+    title: "Services",
     newTab: false,
     submenu: [
       {
-        id: 41,
-        title: "About Page",
-        path: "/about",
-        newTab: false,
-      },
-      {
-        id: 42,
-        title: "Contact Page",
-        path: "/contact",
-        newTab: false,
-      },
-      {
         id: 43,
-        title: "Blog Grid Page",
+        title: "Products",
         path: "/blog",
         newTab: false,
       },
       {
         id: 44,
-        title: "Blog Sidebar Page",
+        title: "Projects",
         path: "/blog-sidebar",
         newTab: false,
       },
       {
+        id: 41,
+        title: "UI Development",
+        path: "/about",
+        newTab: false,
+      },
+      {
+        id: 42,
+        title: "Web Application Development",
+        path: "/contact",
+        newTab: false,
+      },
+      {
         id: 45,
-        title: "Blog Details Page",
+        title: "API Development",
         path: "/blog-details",
         newTab: false,
       },
       {
         id: 46,
-        title: "Sign In Page",
+        title: "Backend Services",
         path: "/signin",
         newTab: false,
       },
       {
         id: 47,
-        title: "Sign Up Page",
+        title: "Maintenance & Support",
         path: "/signup",
         newTab: false,
       },
@@ -80,5 +68,23 @@ const menuData: Menu[] = [
       },
     ],
   },
+  // {
+  //   id: 3,
+  //   title: "Blog",
+  //   path: "/blog",
+  //   newTab: false,
+  // },
+  {
+    id: 33,
+    title: "Careers",
+    path: "/blog",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "Contact",
+    path: "/contact",
+    newTab: false,
+  },  
 ];
 export default menuData;
