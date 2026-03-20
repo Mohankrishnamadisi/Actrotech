57711cb56750f5ad3c5ba273d68686f622586ce0
Mohankrishnamadisi <mohankrishnamadisi@gmail.com>
2026-03-20 22:14:02 +0530

    new changes

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,25 @@
+import Breadcrumb from "@/components/Common/Breadcrumb";
+import Contact from "@/components/Contact";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact Us | Actrotech",
+  description: "Get in touch with Actrotech for your software development needs and inquiries.",
+  // other metadata
+};
+
+const ContactPage = () => {
+  return (
+    <>
+      <Breadcrumb
+        pageName="Contact Page"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+      />
+
+      <Contact />
+    </>
+  );
+};
+
+export default ContactPage;
